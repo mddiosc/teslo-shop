756c0d2cd1c872d77429646423a1f46344d94a1c
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-04-03 17:45:27 +0200

    feat: custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+import { NextPage } from "next";
+import { ShopLayout } from "../components/layouts";
+
+interface Custom404PageProps {}
+
+const Custom404Page: NextPage<Custom404PageProps> = () => {
+  return (
+    <ShopLayout
+      title="Page Not Found"
+      pageDescription="No hay nada que mostrar Aquí"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        sx={{
+          flexDirection: { xs: "column", sm: "row" },
+        }}
+      >
+        <Typography variant="h1" component="h1" fontSize={80} fontWeight={200}>
+          404 |
+        </Typography>
+        <Typography marginLeft={2}>
+          No encontramos ninguna página aquí
+        </Typography>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default Custom404Page;
