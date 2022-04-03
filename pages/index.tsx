20e2e9033721a57d78cc87c618384b4fe321c5d1
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-04-03 18:03:31 +0200

    feat: temporal home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +21,22 @@ const Home: NextPage = () => {
       <Typography variant="h2" sx={{ mb: 1 }}>
         Todos los Productos
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`products/${product.images[0]}`}
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 };
