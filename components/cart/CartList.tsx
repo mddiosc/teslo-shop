47367e609d05618bfe3e934abe8b78d4724b6454
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-04-11 14:34:08 +0200

    feat: cartList component

---
+++ components/cart/CartList.tsx
@@ -0,0 +1,67 @@
+import NextLink from "next/link";
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import { initialData } from "../../database/products";
+import { ItemCounter } from "../ui";
+
+interface CartListProps {}
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+const CartList: React.FC<CartListProps> = () => {
+  return (
+    <>
+      {productsInCart.map((product) => (
+        <Grid container spacing={2} sx={{ mb: 1 }} key={product.slug}>
+          <Grid item xs={3}>
+            <NextLink href="/product/slug" passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: "5px" }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography variant="body1">
+                Talla: <strong>M</strong>
+              </Typography>
+              {/* condicional */}
+              <ItemCounter />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">$ {product.price}</Typography>
+            {/* editable */}
+            <Button variant="text" color="secondary">Eliminar</Button>
+          </Grid>
+        </Grid>
+      ))}
+    </>
+  );
+};
+
+export default CartList;
