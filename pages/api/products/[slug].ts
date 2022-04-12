06135aca32c129b591cb33a47add09569b876705
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-04-12 10:53:39 +0200

    feat: getProduct by slug

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db, SHOP_CONSTANTS } from "../../../database";
+import { Product } from "../../../models";
+import { IProduct } from "../../../interfaces/products";
+
+type Data = { message: string } | IProduct;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return getSlug(req, res);
+
+    default:
+      return res.status(400).json({ message: "Bar request" });
+  }
+}
+
+const getSlug = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { slug } = req.query;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+
+  if (!product) return res.status(404).json({ message: "Product not found" });
+  res.status(200).json(product);
+};
