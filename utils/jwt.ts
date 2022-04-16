f542463426c99fa25fb316f2eb3053cc22cd1d86
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-04-16 14:25:19 +0200

    feat: validate and renew token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -16,3 +16,23 @@ export const signToken = (_id: string, email: string) => {
     }
   );
 };
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error("JWT_SECRET_SEED is not defined");
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || "", (err, payload) => {
+        if (err) {
+          reject("JWT no es válido");
+        }
+        const { _id } = payload as { _id: string };
+        resolve(_id);
+      });
+    } catch (error) {
+      reject("JWT no es válido");
+    }
+  });
+};
