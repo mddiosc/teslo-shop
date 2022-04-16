ece75a82321518f0cd66ef8d21bf4f561b999573
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-04-16 16:21:12 +0200

    feat: validate login form in frontend using react-hook-forms

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,49 +1,89 @@
 import NextLink from "next/link";
 import { NextPage } from "next";
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
 import { AuthLayout } from "../../components/layouts";
+import { validations } from "../../utils/";
 
 interface LoginPageProps {}
 
+type formData = {
+  email: string;
+  password: string;
+};
+
 const LoginPage: NextPage<LoginPageProps> = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formData>();
+
+  const onLoginUser = (data: formData) => {
+    console.log({ data });
+  };
+
   return (
     <AuthLayout title={"Ingresar"}>
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Iniciar Sesión
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Ingresar
-            </Button>
-          </Grid>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Iniciar Sesión
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "Este campo es obligatorio",
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register("password", {
+                  required: "Este campo es obligatorio",
+                  minLength: {
+                    value: 6,
+                    message: "La contraseña debe tener al menos 6 caracteres",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Ingresar
+              </Button>
+            </Grid>
 
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href="/auth/register" passHref>
-              <Link underline="always">¿No tienes cuenta?</Link>
-            </NextLink>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href="/auth/register" passHref>
+                <Link underline="always">¿No tienes cuenta?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
